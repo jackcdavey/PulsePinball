01041f6581585c6ea5012f4d3a943d792b1de38c
jackcdavey <jdavey@scu.edu>
2023-07-22 19:32:37 -0700

    Inital files added

--- Pulse Pinball/GameScene.swift
+++ Pulse Pinball/GameScene.swift
@@ -10,100 +10,108 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    // Declare the pinball, flippers, and launcher
+    var pinball: SKSpriteNode?
+    var leftFlipper: SKSpriteNode?
+    var rightFlipper: SKSpriteNode?
+    var launcher: SKSpriteNode?
+    var leftWall: SKSpriteNode?
+    var rightWall: SKSpriteNode?
+    var outerWall: SKSpriteNode?
+    var curveWall: SKSpriteNode?
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
-    override func sceneDidLoad() {
 
-        self.lastUpdateTime = 0
+    override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // Set up the physics world
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -9.8)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // Initialize the pinball, flippers, and launcher
+        pinball = self.childNode(withName: "pinball") as? SKSpriteNode
+        leftFlipper = self.childNode(withName: "leftFlipper") as? SKSpriteNode
+        rightFlipper = self.childNode(withName: "rightFlipper") as? SKSpriteNode
+        launcher = self.childNode(withName: "launcher") as? SKSpriteNode
+        leftWall = self.childNode(withName: "leftWall") as? SKSpriteNode
+        rightWall = self.childNode(withName: "rightWall") as? SKSpriteNode
+        outerWall = self.childNode(withName: "outerWall") as? SKSpriteNode
+        curveWall = self.childNode(withName: "curveWall") as? SKSpriteNode
+
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
+        // Set up the pinball physics
+        pinball?.physicsBody = SKPhysicsBody(circleOfRadius: pinball!.size.width/2)
+        pinball?.physicsBody?.restitution = 0.5
+        pinball?.physicsBody?.mass = 1.0
+        
+        
+        // Set up the flipper physics
+        leftFlipper?.physicsBody = SKPhysicsBody(rectangleOf: leftFlipper!.size)
+        rightFlipper?.physicsBody = SKPhysicsBody(rectangleOf: rightFlipper!.size)
+        
+        // The flippers should not be affected by gravity or forces
+        leftFlipper?.physicsBody?.isDynamic = false
+        rightFlipper?.physicsBody?.isDynamic = false
+        
+        // The launcher should also not be affected by gravity or forces
+        launcher?.physicsBody = SKPhysicsBody(rectangleOf: launcher!.size)
+        launcher?.physicsBody?.isDynamic = false
+        
+        //Wall physics
+        leftWall?.physicsBody = SKPhysicsBody(rectangleOf: leftWall!.size)
+        rightWall?.physicsBody = SKPhysicsBody(rectangleOf: rightWall!.size)
+        outerWall?.physicsBody = SKPhysicsBody(rectangleOf: outerWall!.size)
+        curveWall?.physicsBody = SKPhysicsBody(rectangleOf: curveWall!.size)
+
+        leftWall?.physicsBody?.isDynamic = false
+        rightWall?.physicsBody?.isDynamic = false
+        outerWall?.physicsBody?.isDynamic = false
+        curveWall?.physicsBody?.isDynamic = false
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        // Get the location of the touch
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+
+        // Print the location of the touch
+        print("Tapped at: \(location)")
+        
+        // Define actions for the flippers and the launcher
+        let moveLeftUp = SKAction.rotate(byAngle: .pi / 4, duration: 0.1)
+        let moveLeftDown = SKAction.rotate(byAngle: -.pi / 4, duration: 0.1)
+        let moveRightUp = SKAction.rotate(byAngle: -.pi / 4, duration: 0.1)
+        let moveRightDown = SKAction.rotate(byAngle: .pi / 4, duration: 0.1)
+        let leftFlip = SKAction.sequence([moveLeftUp, moveLeftDown])
+        let rightFlip = SKAction.sequence([moveRightUp, moveRightDown])
+        let moveLauncherDown = SKAction.moveBy(x: 0, y: -100, duration: 0.5)
+        let moveLauncherUp = SKAction.moveBy(x: 0, y: 110, duration: 0.05)
+        
+        // Apply an impulse to the ball when the launcher moves up
+        let launchBall = SKAction.run {
+            let launch = SKAction.applyImpulse(CGVector(dx: 0, dy: 3000), duration: 0.1)
+            self.pinball?.run(launch)
         }
+        let launchSequence = SKAction.sequence([moveLauncherDown, moveLauncherUp, launchBall])
+
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        // Check where the screen was touched
+        if location.x < -size.width / 6 {
+            // Left third of the screen: flip the left flipper
+            leftFlipper?.run(leftFlip)
+        } else if (location.x > 0 && location.x < size.width / 6) {
+            // Right third of the screen: flip the right flipper
+            rightFlipper?.run(rightFlip)
+        } else {
+            // Middle third of the screen: launch the ball
+            launcher?.run(launchSequence)
+                }
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
-}
+
+
+
+
+
+
--- Pulse Pinball/GameViewController.swift
+++ Pulse Pinball/GameViewController.swift
@@ -14,32 +14,24 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        // Load 'GameScene.sks' as an SKScene.
+        if let scene = SKScene(fileNamed: "GameScene") as? GameScene {
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            if let view = self.view as! SKView? {
+                view.presentScene(scene)
                 
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+                view.ignoresSiblingOrder = true
                 
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
+                view.showsFPS = true
+                view.showsNodeCount = true
             }
         }
     }
+
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
